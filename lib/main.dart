57856bd8d56430e9587bd487f8b12d3296be759f
khairul anas <mafiakanan@gmail.com>
2021-04-18 20:56:07 +0700

    new main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
+import 'package:bloc101/logic/cubits/counter_cubit.dart';
 import 'package:bloc101/logic/cubits/internet_cubit.dart';
-import 'package:bloc101/presentation/screens/home_screen.dart';
-import 'package:bloc101/presentation/screens/second_screen.dart';
-import 'package:bloc101/presentation/screens/third_screen.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'logic/cubits/counter_cubit.dart';
 import 'presentation/routes/app_router.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp(
+    appRouter: AppRouter(),
+    connectivity: Connectivity(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -22,27 +22,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => InternetCubit(connectivity)),
+        BlocProvider<InternetCubit>(create: (_) => InternetCubit(connectivity)),
+        BlocProvider<CounterCubit>(create: (_) => CounterCubit()),
       ],
       child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.yellow,
-          ),
-          routes: {
-            '/': (contex) => MyHomeScreen(
-                  title: 'Home Screen',
-                  color: Colors.blueAccent,
-                ),
-            '/second': (contex) => SecondScreen(
-                  title: 'Second Screen',
-                  color: Colors.redAccent,
-                ),
-            '/third': (contex) => ThirdScreen(
-                  title: 'Third Screen',
-                  color: Colors.greenAccent,
-                ),
-          }),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.yellow,
+        ),
+        onGenerateRoute: appRouter?.onGenerateRoute,
+      ),
     );
   }
 }
