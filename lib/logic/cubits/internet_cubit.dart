f141478eec2067a68cd8da23db4ec87bc9b9b3d2
khairul anas <mafiakanan@gmail.com>
2021-04-18 20:43:56 +0700

    internet cubittt

--- lib/logic/cubits/internet_cubit.dart
+++ lib/logic/cubits/internet_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:bloc101/constants/enums.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
@@ -7,5 +9,35 @@ import 'package:flutter/cupertino.dart';
 part 'internet_state.dart';
 
 class InternetCubit extends Cubit<InternetState> {
-  InternetCubit() : super(InternetLoading());
+  final Connectivity connectivity;
+  StreamSubscription connectivityStreamSubscription;
+  InternetCubit(this.connectivity) : super(InternetLoading());
+
+  StreamSubscription<ConnectivityResult> monitorInternetConnection() {
+    return connectivityStreamSubscription =
+        connectivity.onConnectivityChanged.listen((connectivityResult) {
+      switch (connectivityResult) {
+        case ConnectivityResult.wifi:
+          emitInternetConnected(ConnectionType.Wifi);
+          break;
+        case ConnectivityResult.mobile:
+          emitInternetConnected(ConnectionType.Mobile);
+          break;
+        case ConnectivityResult.none:
+          emitInternetDisconnected();
+          break;
+        default:
+      }
+    });
+  }
+
+  void emitInternetConnected(ConnectionType _connectionType) =>
+      emit(InternetConnected(connectionType: _connectionType));
+  void emitInternetDisconnected() => emit(InternetDisconnected());
+
+  @override
+  Future<void> close() {
+    connectivityStreamSubscription.cancel();
+    return super.close();
+  }
 }
